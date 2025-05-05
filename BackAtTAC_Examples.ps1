5091f4c681f11ae4d97af62f53654d26622e7a61
Ferm1on <michaelcunha57@gmail.com>
2025-05-05 15:43:05 -0400

    Added Examples for users to learn BackATTAC

--- BackAtTAC_Examples.ps1
+++ BackAtTAC_Examples.ps1
@@ -79,7 +79,13 @@ Import-Module BackAtTAC
     $LoadedData = Read-TACData -Path ".\Subnet_0405.csv" -Verbose
     $LoadedData
 
-# Example 4 SETUP
+# EXAMPLE 4
+    # Read file using checksum test
+    $FileChecksum = Get-FileHash -Path ".\Subnet_0405.csv" -Algorithm SHA256
+    $LoadedData = Read-TACData -Path ".\Subnet_0405.csv" -Checksum $FileChecksum.Hash
+    $LoadedData
+
+# Example 5 and 6 SETUP
 
 # dir .\TACBackUp_0405\
 # Mode                 LastWriteTime         Length Name
@@ -90,7 +96,7 @@ Import-Module BackAtTAC
 # la---            5/4/2025 12:44 PM           4327 Switch_0405.csv
 # la---            5/4/2025 12:44 PM          17005 WaP_0405.csv
 
-    # Load all files in the directory into a variable
+    
     $PathCSV = @(
         '.\CivicAddress_0405.csv',
         '.\LocationSchema_0405.csv',
@@ -99,6 +105,9 @@ Import-Module BackAtTAC
         '.\WaP_0405.csv'
     )
 
+# EXAMPLE 5
+
+    # Load all files in the directory into a variable
     $LoadedData = Read-TACData -Path $PathCSV
 
     # Each teams data can be accessed with $DataArray[i]
@@ -106,7 +115,34 @@ Import-Module BackAtTAC
         $DataArray
     }
 
-# EXAMPLE 5 SETUP
+# Example 6
+
+    # Array to store file hashes
+    $All_Hashes = [System.Collections.ArrayList]::new()
+
+    # Build checksum array
+    foreach ($File in $PathCSV) {
+        $FileChecksum = Get-FileHash -Path $File -Algorithm SHA256
+        $All_Hashes.Add($FileChecksum) | Out-Null
+    }
+    
+    # Build only hash array
+    $HashArray = @()
+    
+    foreach ($Hash in $All_Hashes) {
+        $HashArray += $Hash.Hash
+    }
+
+    # Load all files in directory into a variable using checksum test.
+    $LoadedData = Read-TACDAta -Path $PathCSV -Checksum $HashArray
+
+    # Each teams data can be accessed with $DataArray[i]
+    Foreach ($DataArray in $LoadedData) {
+        $DataArray
+    }
+    
+# EXAMPLE 7 SETUP
+
 # dir .\TACBackUp_0405\
 # Mode                 LastWriteTime         Length Name
 # ----                 -------------         ------ ----
@@ -118,8 +154,6 @@ Import-Module BackAtTAC
 
 # la---            5/4/2025 12:44 PM          17005 WaP_0405.csv
 
-    # Load all files in the directory into a variable with non-standard names. 
-    # files and properties array possition should corespond. i.e $Tuble = @($PathCSV[i], $Properties[i])
     $PathCSV = @(
         '.\FooBar1_0405.csv',
         '.\FooBar2_0405.csv',
@@ -136,6 +170,10 @@ Import-Module BackAtTAC
     'WAP'
     )
 
+# EXAMPLE 7
+
+    # Load all files in the directory into a variable with non-standard names. 
+    # files and properties array possition should corespond. i.e $Tuble = @($PathCSV[i], $Properties[i])
     $LoadedData = Read-TACData -Path $PathCSV -Properties $Properties
 
     # Each teams data can be accessed with $DataArray[i]
